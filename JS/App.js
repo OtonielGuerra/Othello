b8e928305c4894bd417f3c041b7a57aa6ef47e06
¨Jonathan <Otonielguerra11@gmail.com>
2018-10-23 11:15:39 -0600

    Creacion de fichas y tablero en pantalla

--- JS/App.js
+++ JS/App.js
@@ -11,15 +11,41 @@ var fondo = {
     cargaOK: false
 };
 
+var fichaBlanca = {
+    url: './imagenes/FichaBlanca.png',
+    image: Image,
+    cargaOK: false
+};
+
+var fichaNegra = {
+    url: './imagenes/FichaNegra.png',
+    image: Image,
+    cargaOK: false
+};
+
 //Codigo que dice que las imagenes son de tipo Image
 fondo.imagen = new Image();
+fichaBlanca.imagen = new Image();
+fichaNegra.imagen = new Image();
 
 //Declaro que la imagen esta en una ruta especifica
 fondo.imagen.src = fondo.url;
+fichaBlanca.imagen.src = fichaBlanca.url;
+fichaNegra.imagen.src = fichaNegra.url;
 
 //Cargar las imagenes
 fondo.imagen.addEventListener("load",function(){
     fondo.cargaOK = true;
+    dibujar();
+});
+
+fichaBlanca.imagen.addEventListener("load",function(){
+    fichaBlanca.cargaOK = true;
+    dibujar();
+});
+
+fichaNegra.imagen.addEventListener("load",function(){
+    fichaNegra.cargaOK = true;
     dibujar();
 });
 
@@ -28,4 +54,13 @@ function dibujar(){
     if(fondo.cargaOK == true){
         lapiz.drawImage(fondo.imagen,0,0);
     }
+
+    if(fichaBlanca.cargaOK == true){
+        lapiz.drawImage(fichaBlanca.imagen,5,5);
+    }
+
+    if(fichaNegra.cargaOK == true){
+        lapiz.drawImage(fichaNegra.imagen,55,55);
+    }
+
     };
