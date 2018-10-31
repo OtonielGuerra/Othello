7cd66e3eb8981e7b6e283f07850ee3df53d68ba6
¨Jonathan <Otonielguerra11@gmail.com>
2018-10-31 09:59:33 -0600

    Cambio a version antigua

--- JS/App.js
+++ JS/App.js
@@ -176,64 +176,30 @@ function dibujar(){
                     fichas();
                     regresar();
                 }
-                break; 
-
+                break;           
             case tecla.ENTER:
+
+            if(matriz[(x -  55)/DIMENSION][(y - 5)/DIMENSION] == 'x'){
+                if(matriz[(x + 45) / DIMENSION][(y - 5) / DIMENSION] == 'x'){
+                    alert("Aqui no puedes poner la ficha amigo");
+                }
+            }
+
+                if(noTurno == true){               
                 if(matriz[(x - 5)/DIMENSION][(y - 5)/DIMENSION] == 'x'){               
                     matriz[(x - 5)/DIMENSION][(y - 5)/DIMENSION] = 'fn';
-                        noTurno = false;                       
+                        noTurno = false;
                         x = 5;
-                        y = 5;        
+                        y = 5;                                      
                         fichas();
-                        regresar();
-                        alert("Turno de la ficha negra");
-                       //Lado Izquierdo Negra
-                       if(matriz[(x -  55)/50][(y - 5)/50] == 'fb'){
-                        for(var i = 2; i < 8; i++){
-                            if(matriz[(x - (i * 50) - 5)/50][(y - 5)/50] == 'fn'){
-                                for(var j = 1; i < 8; j++){
-                                    if(matriz[(x - (j * 50) - 5)/50][(y - 5)/50] =='fn'){
-                                        x = 5;
-                                        y = 5; 
-                                        fichas();
-                                        regresar();
-                                        if(matriz[3][4] = 'fn'){
-                                            SW = false;
-                                        }
-                                    }
-                                    matriz[(x - (j * 50) - 5)/50][(y - 5)/50] = 'fn';
-                                }
-                            }
-                        }
-                    }
-                    //Lado Derecho Negra
-                    if(matriz[(x + 45) / 50][(y - 5) / 50] == 'fb'){
-                        for(var i = 2; i < 8; i++){
-                            if(matriz[(x + (i * 50) - 5) / 50][(y - 5) / 50] == 'fn'){
-                                for(var j = 1; i < 8; j++){
-                                    if(matriz[(x + (j * 50) - 5)/50][(y - 5)/50] == 'fn'){  
-                                        x = 5;
-                                        y = 5;                                      
-                                        fichas();
-                                        regresar();
-                                        if(matriz[4][3] = 'fn'){
-                                            SW = false;
-                                        }
-                                        break;
-                                    }
-                                    matriz[(x + (j * 50) - 5)/50][(y - 5)/50] ='fn';
-                                }
-                            }
-                        }
-                    }
-                    matriz[(x - 5)/DIMENSION][(y - 5)/DIMENSION] = 'fn';
-                    noTurno = false;
-                    x = 5;
-                    y = 5;     
-                    fichas();
-                    regresar();
-                    alert("Camio de turno");
-        }  
+                        alert("Turno de la ficha blanca");
+                }
+
+            }else if(noTurno == false){
+                    if(matriz[(x - 5)/DIMENSION][(y - 5)/DIMENSION] == 'x'){               
+                        matriz[(x - 5)/DIMENSION][(y - 5)/DIMENSION] = 'fb';
+                        noTurno = true;
+                            //Lado Izquierdo
                             if(matriz[(x -  55)/DIMENSION][(y - 5)/DIMENSION] == 'fn'){
                                 for(var i = 2; i < 8; i++){
                                     if(matriz[(x - (i * DIMENSION) - 5)/DIMENSION][(y - 5)/DIMENSION] == 'fb'){
@@ -253,7 +219,7 @@ function dibujar(){
                                     }
                                 }
                             }
-                            //Lado Derecho Blanca
+                            //Lado Derecho
                             if(matriz[(x + 45) / DIMENSION][(y - 5) / DIMENSION] == 'fn'){
                                 for(var i = 2; i < 8; i++){
                                     if(matriz[(x + (i * DIMENSION) - 5) / DIMENSION][(y - 5) / DIMENSION] == 'fb'){
@@ -273,12 +239,13 @@ function dibujar(){
                                     }
                                 }
                             }
-                        x = 5;
-                        y = 5;      
-                        regresar();  
-                        fichas();
-                        alert("Camio de turno");
+                            x = 5;
+                            y = 5;        
+                            fichas();
+                            alert("Turno de la ficha negra");
                 }
             }
+            fichas();
             break;
-    
+    }
+}
