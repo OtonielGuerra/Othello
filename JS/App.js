590d0d14841c46757c0576a988a79084933aa5b8
¨Jonathan <Otonielguerra11@gmail.com>
2018-11-07 15:21:38 -0600

    Actualizacion 0.75

--- JS/App.js
+++ JS/App.js
@@ -313,11 +313,12 @@ function Movida(x, y, F1, F2, color) {
             }
         }
         //Diagonal abajo izquierda
-        if (((y + 45) / DIMENSION) < 8 && ((x - 45) / DIMENSION) >= 0) {
+        if (((y + 45) / DIMENSION) < 8 && ((x - 55) / DIMENSION) >= 0) {
             if (matriz[(x - 55) / DIMENSION][(y + 45) / DIMENSION] == F2) {
                 for (var i = 2; i < 8; i++) {
                     if (((y + (i * DIMENSION) - 5) / DIMENSION) < 8 && ((x - (i * DIMENSION) - 5) / DIMENSION) >= 0) {
                         if (matriz[(x - (i * DIMENSION) - 5) / DIMENSION][(y + (i * DIMENSION) - 5) / DIMENSION] == F1) {
+                            console.log("Hola amigo");
                             for (var j = 1; i < 8; j++) {
                                 if (matriz[(x - (j * DIMENSION) - 5) / DIMENSION][(y + (j * DIMENSION) - 5) / DIMENSION] == F1) {
                                     break;
